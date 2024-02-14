9cdfb82011e3ad3145820b7d92d94291bcaffc69
Yogesh-Kumar21 <yogelale64@gmail.com>
2024-02-14 17:11:11 +0530

    inital commit

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Card = (props) =>{
+    return(
+        <div className=" tc bg-light-green dib br3 pa3 ma2 .b-solid bw5 shadow-5 grow">
+            <img alt='robots' src= {`https://robohash.org/${props.id}?200*200`} />
+            <div>
+                <h2>{props.name}</h2>
+                <p>{props.email}</p>
+            </div>
+        </div>
+
+    );
+}
+export default Card;
---
+++ src/Robots.js
@@ -0,0 +1,63 @@
+export const robots = [
+    {
+      id: 1,
+      name: 'Aditiya Rana',
+      username: 'Bret',
+      email: 'Adi@april.biz'
+    },
+    {
+      id: 2,
+      name: 'Om Pachauli',
+      username: 'Antonette',
+      email: 'Om@melissa.tv'
+    },
+    {
+      id: 3,
+      name: 'Vishal',
+      username: 'Samantha',
+      email: 'Vishal@yesenia.net'
+    },
+    {
+      id: 4,
+      name: 'Yuvika',
+      username: 'Karianne',
+      email: 'Julianne.OConner@kory.org'
+    },
+    {
+      id: 5,
+      name: 'Utkarsh Saxena',
+      username: 'Kamren',
+      email: 'Lucio_Hettinger@annie.ca'
+    },
+    {
+      id: 6,
+      name: 'Vidhi',
+      username: 'Leopoldo_Corkery',
+      email: 'Karley_Dach@jasper.info'
+    },
+    {
+      id: 7,
+      name: 'Banta',
+      username: 'Elwyn.Skiles',
+      email: 'Telly.Hoeger@billy.biz'
+    },
+    {
+      id: 8,
+      name: 'Saktiman',
+      username: 'Maxime_Nienow',
+      email: 'Sherwood@rosamond.me'
+    },
+    {
+      id: 9,
+      name: 'Sanjana',
+      username: 'Delphine',
+      email: 'Chaim_McDermott@dana.io'
+    },
+    {
+      id: 10,
+      name: 'Yash',
+      username: 'Moriah.Stanton',
+      email: 'Rey.Padberg@karina.biz'
+    }
+  ];
+  
